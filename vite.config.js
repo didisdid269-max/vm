e72f48c35bcaba35048bde8f5044965521059a84
didisdid269-max <didisdid269@gmail.com>
2026-05-20 16:49:04 -0400

    Remove base44 plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import base44 from '@base44/sdk/vite'
 
 export default defineConfig({
   logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true,
-    }),
-    react(),
-  ],
+  plugins: [react()],
 })
